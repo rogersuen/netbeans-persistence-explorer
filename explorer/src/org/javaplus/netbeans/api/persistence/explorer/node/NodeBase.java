0ce96c7f3bf79c1bdfb3f08029eeecd77a933108
Roger Suen <rogersuen@live.com>
2010-06-07 07:59:20 +0000

    general cleanup, fixed lookuplistener bug, added javadoc
    git-svn-id: http://www.javaplus.org/svn/javaplus/javaplus/netbeans/persistence/trunk@60 3f2202ac-3770-d442-90ff-cc26b0a2c708

--- explorer/src/org/javaplus/netbeans/api/persistence/explorer/node/NodeBase.java
+++ explorer/src/org/javaplus/netbeans/api/persistence/explorer/node/NodeBase.java
@@ -1,18 +1,18 @@
 /*
- * @(#)NodeBase.java   10/04/28
- * 
+ * @(#)NodeBase.java   10/06/07
+ *
  * Copyright (c) 2010 Roger Suen(SUNRUJUN)
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
  *
  */
+
 package org.javaplus.netbeans.api.persistence.explorer.node;
 
-import javax.swing.event.ChangeEvent;
 import org.openide.nodes.AbstractNode;
 import org.openide.nodes.ChildFactory;
 import org.openide.nodes.Children;
@@ -20,6 +20,8 @@ import org.openide.nodes.Node;
 import org.openide.util.Lookup;
 import org.openide.util.LookupEvent;
 import org.openide.util.LookupListener;
+import org.openide.util.lookup.AbstractLookup;
+import org.openide.util.lookup.InstanceContent;
 import org.openide.util.lookup.Lookups;
 import org.openide.util.lookup.ProxyLookup;
 
@@ -32,38 +34,75 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import javax.swing.Action;
+import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
-import org.openide.util.lookup.AbstractLookup;
-import org.openide.util.lookup.InstanceContent;
 
 /**
- * TODO: javadoc
+ * <p>
+ * The base class for all Persistence Explorer nodes.</p>
+ * <p>
+ * All persistence explorer nodes are registered in the system filesystem
+ * via layer files. The parent folder (the base node folder) is defined by
+ * the constant {@link #LAYER_FOLDER_BASE}. Each node should be registered
+ * in a child folder under it with thier unique name returned from the
+ * {@link #getLayerFolder() } method. </p>
+ * <p>
+ * For a specific node, its child node instances or provider factory instance
+ * and actions are registered in the subfolders with the names defined by
+ * {@link #LAYER_FOLDER_CHILDREN} and {@link #LAYER_FOLDER_ACTIONS}
+ * respectively, under the node's folder. These child nodes are then
+ * registered via their own folders under the base node folder.</p>
+ *
  * @author Roger Suen
  */
 public abstract class NodeBase extends AbstractNode {
 
-    public static final String LAYER_PATH_BASE = "Persistence/Explorer/Nodes/";
-    public static final String FOLDER_CHILDREN = "/Children";
-    public static final String FOLDER_ACTIONS = "/Actions";
-
-    protected static final String FOLDER_ICON_BASE =
-            "org/javaplus/netbeans/persistence/resources/folder.gif";
     /**
-     * The singleton logger
+     * The constant holding the name of the base folder in the system
+     * filesystem in which all persistence explorer nodes are registered,
+     * <tt>{@value #LAYER_FOLDER_BASE}</tt>.
+     * @see #getLayerFolder()
      */
-    private static final Logger logger =
-            Logger.getLogger(NodeBase.class.getName());
-    private final String childrenLayerFolder = LAYER_PATH_BASE + getLayerFolder()
-            + FOLDER_CHILDREN;
-    private final String actionsLayerFolder = LAYER_PATH_BASE
-            + getLayerFolder()
-            + FOLDER_ACTIONS;
-    private final ChildRegistry childRegistry;
-    private final ActionRegistry actionRegistry;
+    public static final String LAYER_FOLDER_BASE =
+        "Persistence/Explorer/Nodes/";
+
     /**
-     * 
+     * The constant holding the name of the subfolder, in which child nodes
+     * of a node are registered, <tt>{@value #LAYER_FOLDER_CHILDREN}</tt>.
+     * @see #getLayerFolder()
+     */
+    public static final String LAYER_FOLDER_CHILDREN = "/Children";
+
+    /**
+     * The constant holding the name of the subfolder, in which actions
+     * of a node are registered, <tt>{@value #LAYER_FOLDER_ACTIONS}</tt>.
+     * @see #getLayerFolder()
+     */
+    public static final String LAYER_FOLDER_ACTIONS = "/Actions";
+
+    /**
+     * The base name of the default folder icon.
+     */
+    protected static final String DEFAULT_FOLDER_ICON_BASE =
+        "org/javaplus/netbeans/api/persistence/explorer/node/resources/"
+        + "defaultFolder.gif";
+
+    //
+    // Private constants
+    //
+    private static final Logger logger =
+        Logger.getLogger(NodeBase.class.getName());
+
+    /**
+     * The proxy lookup instance of this node.
      */
     protected final NodeLookup lookup;
+
+    //
+    // Private fields
+    //
+    private final ChildRegistry childRegistry;
+    private final ActionRegistry actionRegistry;
 
     /**
      * Sole constructor.
@@ -78,7 +117,6 @@ public abstract class NodeBase extends AbstractNode {
      */
     private NodeBase(NodeLookup nodeLookup) {
         super(Children.LEAF, nodeLookup);
-
         lookup = nodeLookup;
         childRegistry = new ChildRegistry();
         actionRegistry = new ActionRegistry();
@@ -86,27 +124,32 @@ public abstract class NodeBase extends AbstractNode {
     }
 
     /**
-     * 
-     * @return
+     * Called when the child registry changed, or any child provider changed.
      */
-    protected abstract String getLayerFolder();
-
-    public void update() {
+    private void update() {
         childRegistry.refresh();
         updateProperties();
     }
 
-    protected void updateProperties() {
-    }
+    /**
+     * <p>
+     * Returns the name of the folder, in which this node is registered. </p>
+     * <p>
+     * All persistence explorer nodes are registered in the system filesystem
+     * via layer files. The parent folder is defined by the constant
+     * {@link #LAYER_FOLDER_BASE}. Each node should be registered in a child
+     * folder under it with thier unique name returned from this method.</p>
+     *
+     * @return the relative layer folder name, without any leading or trailing
+     *         '/' separator.
+     */
+    protected abstract String getLayerFolder();
 
     /**
-     * 
-     * @param lookup
+     * This method is called whenever the child nodes are refreshed due to
+     * the registry change.
      */
-    @Deprecated
-    protected void addLookup(Lookup lookup) {
-        this.lookup.addLookup(lookup);
-    }
+    protected void updateProperties() {}
 
     @Override
     public Action[] getActions(boolean context) {
@@ -119,7 +162,7 @@ public abstract class NodeBase extends AbstractNode {
         // insert our actions before other actions returned from super
         Action[] contextActions = super.getActions(context);
         List<Action> list = new ArrayList<Action>(actions.length
-                + contextActions.length);
+                                + contextActions.length);
         Collections.addAll(list, actions);
         Collections.addAll(list, contextActions);
         return list.toArray(new Action[list.size()]);
@@ -165,56 +208,64 @@ public abstract class NodeBase extends AbstractNode {
      *  <li>Node/NodeProvider registry changes</li>
      *  <li>Any NodeProvider changes</li>
      * </ul>
-     * 
-     * 
+     *
+     *
      * @see #getLayerFolder()
-     * @see #update() 
+     * @see #update()
      */
     private final class ChildRegistry extends ChildFactory<Object> {
-
+        private final String childrenFolder = LAYER_FOLDER_BASE
+                                              + getLayerFolder()
+                                              + LAYER_FOLDER_CHILDREN;
         private final Lookup.Result<Object> childrenLookupResult;
+        private final LookupListener lookupListener;
+
         /**
-         * List of Node/NodeProvider instances registered in the XML
-         * layer folder
+         * List of Node/NodeProviderFactory instances registered in the XML
+         * layer folder.
          */
-        private final List<Object> children = new CopyOnWriteArrayList<Object>();
+        private final List<Object> children =
+            new CopyOnWriteArrayList<Object>();
 
-        public ChildRegistry() {
-            childrenLookupResult = Lookups.forPath(childrenLayerFolder).
-                    lookupResult(Object.class);
-            childrenLookupResult.addLookupListener(new LookupListener() {
-
+        private ChildRegistry() {
+            childrenLookupResult =
+                Lookups.forPath(childrenFolder).lookupResult(Object.class);
+            lookupListener = new LookupListener() {
                 public void resultChanged(LookupEvent ev) {
-                    if (logger.isLoggable(Level.FINE)) {
-                        logger.log(Level.FINE,
-                                "Lookup.Result(Object.class) change detected "
-                                + "in the folder: {0}",
-                                childrenLayerFolder);
+                    if (logger.isLoggable(Level.FINER)) {
+                        logger.log(
+                            Level.FINER,
+                            "Lookup.Result(Object.class) change detected "
+                            + "in the folder: [{0}]", childrenFolder);
                     }
 
-                    // reload the registry and then update this node
+                    // reload the registry
                     loadRegistry();
-                    update(); // NodeBase.update()
-                }
-            });
 
+                    // update this node
+                    update();
+                }
+            };
+            childrenLookupResult.addLookupListener(lookupListener);
+
+            // constructing, need not update
             loadRegistry();
         }
 
         private synchronized void loadRegistry() {
-            if (logger.isLoggable(Level.FINE)) {
-                logger.log(Level.FINE,
-                        "Loading instances of Node/NodeProvider from "
-                        + "the folder {0}",
-                        childrenLayerFolder);
+            if (logger.isLoggable(Level.FINER)) {
+                logger.log(
+                    Level.FINER,
+                    "Loading Node/NodeProviderFactory from the folder [{0}]",
+                    childrenFolder);
             }
 
             // clear, and then iterate to add, so synchronized
             children.clear();
 
-            // load all instances of Node and NodeProvider
+            // load all instances of Node and NodeProviderFactory
             Collection<? extends Object> objects =
-                    childrenLookupResult.allInstances();
+                childrenLookupResult.allInstances();
             for (Object object : objects) {
                 if (object instanceof Node) {
                     children.add(object);
@@ -225,27 +276,27 @@ public abstract class NodeBase extends AbstractNode {
                     // listens to the provider for the change event
                     // update the node on changes
                     provider.addChangeListener(new ChangeListener() {
-
                         public void stateChanged(ChangeEvent e) {
-                            update(); // NodeBase.update()
+                            update();    // NodeBase.update()
                         }
                     });
                     children.add(provider);
                 } else if (logger.isLoggable(Level.WARNING)) {
-                    logger.log(Level.WARNING,
-                            "Node/NodeProvider instance expected in the "
-                            + "folder {0}, but {1} found there", new Object[]{
-                                childrenLayerFolder,
-                                object});
+                    logger.log(
+                        Level.WARNING,
+                        "Node/NodeProviderFactory instances expected in "
+                        + "the folder [{0}], but an instance [{1}] "
+                        + "found there.", new Object[] { childrenFolder,
+                            object });
                 }
             }
 
-            if (logger.isLoggable(Level.FINE)) {
-                logger.log(Level.FINE,
-                        "{0} instance(s) of Node/NodeProvider loaded "
-                        + "from the folder {1}", new Object[]{
-                            children.size(),
-                            childrenLayerFolder});
+            if (logger.isLoggable(Level.FINER)) {
+                logger.log(Level.FINER,
+                           "{0} Node/NodeProviderFactory instance(s) loaded "
+                           + "from the folder [{1}]", new Object[] {
+                               children.size(),
+                               childrenFolder });
             }
         }
 
@@ -266,39 +317,45 @@ public abstract class NodeBase extends AbstractNode {
                     toPopulate.addAll(((NodeProvider) object).getNodes());
                 }
             }
+
             return true;
         }
 
         @Override
         protected Node[] createNodesForKey(Object key) {
             if (key instanceof Node) {
-                return new Node[]{(Node) key};
+                return new Node[] { (Node) key };
             } else {
-                return null; // should never happen
+                return null;    // should never happen
             }
         }
     }
+
 
     /**
      * Helper used to access actions registered in corresponding layer folder
      * of the node.
      */
     private final class ActionRegistry {
-
+        private final String actionsFolder = LAYER_FOLDER_BASE
+                                             + getLayerFolder()
+                                             + LAYER_FOLDER_ACTIONS;
         private final Lookup.Result<Object> lookupResult;
+        private final LookupListener lookupListener;
         private final List<Action> actions = new CopyOnWriteArrayList<Action>();
 
         /**
          * Constructor
          */
         public ActionRegistry() {
-            lookupResult = Lookups.forPath(actionsLayerFolder).lookupResult(Object.class);
-            lookupResult.addLookupListener(new LookupListener() {
-
+            lookupResult =
+                Lookups.forPath(actionsFolder).lookupResult(Object.class);
+            lookupListener = new LookupListener() {
                 public void resultChanged(LookupEvent ev) {
                     loadActions();
                 }
-            });
+            };
+            lookupResult.addLookupListener(lookupListener);
             loadActions();
         }
 
@@ -313,7 +370,7 @@ public abstract class NodeBase extends AbstractNode {
         /**
          * Clear the internal action list, and then reload it from the layer
          */
-        private void loadActions() {
+        private synchronized void loadActions() {
             actions.clear();
             Collection<? extends Object> objects = lookupResult.allInstances();
             for (Object object : objects) {
@@ -324,27 +381,27 @@ public abstract class NodeBase extends AbstractNode {
                 } else if (logger.isLoggable(Level.WARNING)) {
                     logger.log(Level.WARNING,
                             "Action/JSeparator instance expected in the layer "
-                            + "folder {0}, but {1} found there", new Object[]{
-                                actionsLayerFolder,
-                                object});
+                            + "folder {0}, but {1} found there", new Object[] {
+                                actionsFolder,
+                                object });
                 }
             }
 
             if (logger.isLoggable(Level.FINE)) {
                 logger.log(Level.FINE,
-                        "{0} instance(s) of Action/JSeparator loaded "
-                        + "from layer folder {1}", new Object[]{
-                            actions.size(),
-                            actionsLayerFolder});
+                           "{0} instance(s) of Action/JSeparator loaded "
+                           + "from layer folder {1}", new Object[] {
+                               actions.size(),
+                               actionsFolder });
             }
         }
     }
 
-    /**
-     * 
-     */
-    public static final class NodeLookup extends ProxyLookup {
 
+    /**
+     * A proxy lookup implementation used by persistence explorer nodes.
+     */
+    protected static final class NodeLookup extends ProxyLookup {
         private final InstanceContent instanceContent = new InstanceContent();
 
         public NodeLookup() {
@@ -358,11 +415,11 @@ public abstract class NodeBase extends AbstractNode {
         public void addLookup(Lookup... lookups) {
             Lookup[] originalLookups = getLookups();
             Lookup[] newLookups =
-                    new Lookup[originalLookups.length + lookups.length];
+                new Lookup[originalLookups.length + lookups.length];
             System.arraycopy(originalLookups, 0, newLookups, 0,
-                    originalLookups.length);
+                             originalLookups.length);
             System.arraycopy(lookups, 0, newLookups, originalLookups.length,
-                    lookups.length);
+                             lookups.length);
             setLookups(newLookups);
         }
     }
